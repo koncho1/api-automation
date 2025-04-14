73994463d670e09f594ac4847b5899c99cd36b63
koncho1 <xvx2402@gmail.com>
2025-04-14 17:57:29 +0200

    added validation against schema

--- src/test/java/org/example/CommentsTest.java
+++ src/test/java/org/example/CommentsTest.java
@@ -1,9 +1,12 @@
 package org.example;
 
-import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+
+import com.networknt.schema.ValidationMessage;
 import org.apache.hc.client5.http.ClientProtocolException;
+import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.*;
 import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import org.apache.hc.client5.http.impl.classic.BasicHttpClientResponseHandler;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
@@ -17,52 +20,58 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CommentsTest {
+public class CommentsTest extends BaseTests {
     @Test
-    public void getComments() throws ClientProtocolException, IOException {
+    public void getComments() throws IOException {
+        HttpClient client = HttpClients.createDefault();
         HttpUriRequest request=new HttpGet("https://gorest.co.in/public/v2/comments");
-        HttpResponse httpResponse= HttpClientBuilder.create().build().execute(request);
-        Assert.assertEquals(httpResponse.getCode(),200);
+        String response = client.execute(request, new BasicHttpClientResponseHandler());
+        List <ValidationMessage> errors= super.validatePayload(response, "getCommentsTemplate");
+        Assert.assertTrue(errors.isEmpty());
     }
 
     @Test
-    public void getComment() throws ClientProtocolException, IOException, ProcessingException {
-        HttpUriRequest request = new HttpGet("https://gorest.co.in/public/v2/comments/147013");
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        Assert.assertEquals(httpResponse.getCode(), 200);
+    public void getComment() throws IOException{
+        HttpClient client = HttpClients.createDefault();
+        HttpUriRequest request=new HttpGet("https://gorest.co.in/public/v2/comments/147195");
+        String response = client.execute(request, new BasicHttpClientResponseHandler());
+        List <ValidationMessage> errors= super.validatePayload(response, "getCommentTemplate");
+        Assert.assertTrue(errors.isEmpty());
     }
 
     @Test
     public void postComment() throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpClient client = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost("https://gorest.co.in/public/v2/comments?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-        params.add(new BasicNameValuePair("post_id", "203565"));
+        params.add(new BasicNameValuePair("post_id", "203766"));
         params.add(new BasicNameValuePair("name", "John Doe"));
         params.add(new BasicNameValuePair("email", "mail@gmail.example"));
         params.add(new BasicNameValuePair("body", "comment"));
         httpPost.setEntity(new UrlEncodedFormEntity(params));
-        HttpResponse response = httpclient.execute(httpPost);
-        Assert.assertEquals(response.getCode(), 201);
+        String response = client.execute(httpPost, new BasicHttpClientResponseHandler());
+        List <ValidationMessage> errors= super.validatePayload(response, "postCommentTemplate");
+        Assert.assertTrue(errors.isEmpty());
     }
 
     @Test
     public void putComment() throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpPut httpPut = new HttpPut("https://gorest.co.in/public/v2/comments/147013?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPut httpPut = new HttpPut("https://gorest.co.in/public/v2/comments/147193?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
         List<NameValuePair> params = new ArrayList<NameValuePair>(2);
         params.add(new BasicNameValuePair("post_id", "203565"));
         params.add(new BasicNameValuePair("name", "John Doe"));
         params.add(new BasicNameValuePair("email", "mail@gmail.example"));
         params.add(new BasicNameValuePair("body", "comment"));
         httpPut.setEntity(new UrlEncodedFormEntity(params));
-        HttpResponse response = httpclient.execute(httpPut);
-        Assert.assertEquals(response.getCode(), 200);
+        String response = client.execute(httpPut, new BasicHttpClientResponseHandler());
+        List <ValidationMessage> errors= super.validatePayload(response, "putCommentTemplate");
+        Assert.assertTrue(errors.isEmpty());
     }
 
     @Test
     public void deleteComment() throws IOException {
-        HttpUriRequest request = new HttpDelete("https://gorest.co.in/public/v2/comments/146982?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
+        HttpUriRequest request = new HttpDelete("https://gorest.co.in/public/v2/comments/147190?access-token=5d034ab1ba63642fd87b6ff1133eb5fbadad18da23c7c44bbe1f857e25b733b7");
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
         Assert.assertEquals(httpResponse.getCode(),204);
 
